22ebc5fba5c91ea00da0a965ebc6846ab55281cc
jcurless <jcurless@asu.edu>
2014-02-20 21:28:16 -0700

    Re-push

---
+++ HelloWorld.c
@@ -0,0 +1,17 @@
+/* Hello World program
+	
+	CSE 220 T/TH 10:30
+	Jackson Curless
+	Dylan Coyle
+	Phillip Day
+*/
+
+#include <stdio.h> 
+
+int main() //program starts at main
+{
+	printf("Hello World!\n"); //prints "Hello World!" to the console with a return
+	printf("CSE 220 is awesome!\n"); //prints "CSE 220 is awesome!" to the console
+	printf("This is some text. \n"); //prints some text;)
+	return 0;
+}
