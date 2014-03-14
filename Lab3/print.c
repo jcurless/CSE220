f73a8814b40508b164e10e7c8a77c346a7d88f7f
phillipday <phillip.day@asu.edu>
2014-03-13 21:37:56 -0700

    add print token

--- Lab3/print.c
+++ Lab3/print.c
@@ -5,6 +5,7 @@
 //  Created by Bryce Holton.
 //  Copyright (c) 2014 Bryce Holton. All rights reserved.
 //
+//	Phillip Day, Jackson Curless, Dylan Coyle
 
 #include "print.h"
 
@@ -59,5 +60,28 @@ static void print_page_header(char source_name[], char date[])
 }
 void print_token(Token *token)
 {
+	if(token->code == IDENTIFIER) 
+	{
+        printf("     >> %17s%s", SYMBOL_STRINGS[token->code], token->identifier);
+    }
+    else if(token->code == NUMBER) 
+	{
+        if(token->type == INTEGER_LIT) 
+		{
+            printf("     >> %17s%d", SYMBOL_STRINGS[token->code], token->int_lit);
+        }
+        else if(token->type == REAL_LIT) 
+		{
+            printf("     >> %17s%f", SYMBOL_STRINGS[token->code], token->real_lit);
+        }
+    }
+    else if(token->code == STRING) 
+	{
+        printf("     >> %17s%s", SYMBOL_STRINGS[token->code], token->str_lit);
+    }
+    else 
+	{
+        printf("     >> %17s%s", SYMBOL_STRINGS[token->code], downshift_word(SYMBOL_STRINGS[token->code]));
+    }
     //Missing code goes here
 }
