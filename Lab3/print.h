a46fb256dfeb173bb00e77959f887e3fd08b7494
phillipday <phillip.day@asu.edu>
2014-03-13 21:35:41 -0700

    edit header

--- Lab3/print.h
+++ Lab3/print.h
@@ -5,6 +5,7 @@
 //  Created by Bryce Holton.
 //  Copyright (c) 2014 Bryce Holton. All rights reserved.
 //
+//	Phillip Day, Jackson Curless, Dylan Coyle
 
 #ifndef Lab3_print_h
 #define Lab3_print_h
