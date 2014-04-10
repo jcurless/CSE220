bc78f3da85660da92497150c023c5d2636a6dd7f
jcurless <jcurless@asu.edu>
2014-04-10 06:57:34 -0700

    initial commit

---
+++ Lab4/LinkList.h
@@ -0,0 +1,39 @@
+//
+//  LinkList.h
+//  Lab4
+//  Phillip Day, Jackson Curless, Dylan Coyle
+//
+//  LinkList.h header file to implement LinkedList object.
+//
+
+#ifndef LINKLIST_H_INCLUDED
+#define LINKLIST_H_INCLUDED
+
+
+#include <iostream>
+#include "common.h"
+
+
+using namespace std;
+
+class LinkList
+{
+
+    private:
+
+    int liner;
+    LinkedList *nextLine;
+    LinkedList *current;
+
+    public:
+    LinkList();
+    ~LinkList();
+    void addTo(int new_line);
+    void printTheList();
+    void deleteTheList();
+    void setter(int line_num);
+
+    LinkedList* getNext();
+    int getLine();
+};
+#endif // LINKLIST_H_INCLUDED
---
+++ Lab4/Print.h
@@ -0,0 +1,35 @@
+//
+//  Print.h
+//  Lab4
+//  Phillip Day, Jackson Curless, Dylan Coyle
+//
+//  Created by Bryce Holton.
+//
+
+#ifndef __Lab4__Print__
+#define __Lab4__Print__
+
+#include <iostream>
+#include "common.h"
+#include "Token.h"
+
+using namespace std;
+
+class Print
+{
+private:
+    string sourceFileName;
+    string currentDate;
+    int pageNumber;
+
+    void printPageHeader();
+
+public:
+    Print(char source_name[], char date[]);
+    ~Print();
+
+    void printLine(char line[]);
+    void printToken(Token *token);
+};
+
+#endif /* defined(__Lab4__Print__) */
